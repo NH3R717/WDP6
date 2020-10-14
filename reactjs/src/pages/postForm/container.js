87cd116898d0addc8c79f87e178055f7a2116dcd
“Tommy” <nh3r717@gmail.com>
2020-10-14 16:45:13 -0400

    update – container postForm

--- reactjs/src/pages/postForm/container.js
+++ reactjs/src/pages/postForm/container.js
@@ -1,27 +1,19 @@
 import { connect } from 'react-redux';
-import {
-//   the pertinent functions (getThing, addThing)
-} from 'the store action file';
-// } from '../../store/place/actions';
+import { getOneByIdPost, addPost, deletePost } from '../../store/posts/actions';
 
 function mapStateToProps(state, props) {
+  // get the id from the route params
+  const { match: { params: { id } } } = props;
+  const {
+    posts: {
+      byId: {
+        [id]: posts,
+      },
+    },
 
-const mapDispatchToProps = {
-//   the pertinent functions (getThing, addThing)
-};
-
-//     function mapStateToProps(state, props) {
-//   const { match: { params: { id } } } = props;
-//   const {
-//     choices: {
-//       byId: {
-//         [id]: choice,
-//       },
-//     },
-//   } = state;
-//   return { choice };
-// }
-    
-      return { choice };
+  } = state;
+  const posts = posts.map(postId => byId[postId]);
+  return { user };
 }
+const mapDispatchToProps = { getOneByIdPost, addPost, deletePost };
 export default connect(mapStateToProps, mapDispatchToProps);
