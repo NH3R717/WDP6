e050b7d7b17613b19688fa75d425b356404d8185
“Tommy” <nh3r717@gmail.com>
2020-10-14 12:16:55 -0400

    update – post actions

--- reactjs/src/store/posts/actions.js
+++ reactjs/src/store/posts/actions.js
@@ -1,14 +1,44 @@
 import API from '../../../../api';
 import {
-  SET_POSTS
+    SET_POST,
+    SET_POSTS,
+    ADD_POST,
+    DELETE_POST,
 } from '../actionsTypes';
 import { shouldLoad } from '../utils';
 
-// ! export (see example below)
+export const getAllPosts = () => async (dispatch, getState) => {
+  const { posts: { userPostsLoadedAt } } = getState();
+  if (!shouldLoad(userPostsLoadedAt)) return;
+  const userPosts = await API.get('/posts');
+  dispatch({ type: SET_POSTS, userPosts });
+};
 
-export const getPosts= postId => async (dispatch, getState) => {
-  const { posts: { questionLoadedAt: { [postId]: loadedAt = 0 } } } = getState();
-  if (!shouldLoad(loadedAt)) return;
-  const posts= await API.get(`/post?postId=${postId}`);
-  dispatch({ type: SET_POSTS, posts, postId });
+export const getOneByIdPost = id => async (dispatch, getState) => {
+  const { post: { byId: { [id]: existingPost } } } = getState();
+dispatch(getPostSelect(id));
+  if (existingPost) return;
+  const post = await API.get(`/posts/${id}`);
+  dispatch({ type: SET_POST, post });
+};
+
+export const deletePost = id => async (dispatch) => {
+  await API.delete(`posts/${id}`);
+  dispatch({ type: DELETE_POST, id });
+};
+
+export const addPost = (post) => async (dispatch) => {
+  if (post.id) {
+    const updatePost = await API.put(`/posts/${post.id}`, post);
+    dispatch({ type: SET_POST, post: { ...post, ...updatePost } });
+  } else {
+    const newPost = await API.post("/posts", post);
+    console.log(`New Post! ${{ ...newPost }}`);
+    dispatch({ type: SET_POST, post: { ...post, ...newPost } });
+    dispatch({
+      type: ADD_POST,
+      id: newPost.id,
+      postId: newPost.postId,
+    });
+  }
 };
--- reactjs/src/store/posts/reducer.js
+++ reactjs/src/store/posts/reducer.js
@@ -1,7 +1,11 @@
+import API from '../../../../api';
 import {
-  SET_POST,
-} from '../actionTypes';
-import { arrayToObject, removeIdFromObject, removeIdFromArray } from '../utils';
+    SET_POST,
+    SET_POSTS,
+    ADD_POST,
+    DELETE_POST,
+} from '../actionsTypes';
+import { shouldLoad } from '../utils';
 
 const startState = {
   postLoadedAt: {},
@@ -9,7 +13,7 @@ const startState = {
   byId: {},
 };
 
-export default function choiceReducer(state = startState, action) {
+export default function postReducer(state = startState, action) {
   const { type, ...payload } = action;
     switch (type) {
     //   ! reducer functions go here
