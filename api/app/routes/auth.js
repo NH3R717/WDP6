53013f6296ff10287a68e2ff17322f4f070b2d7a
“Tommy” <nh3r717@gmail.com>
2020-10-15 18:30:57 -0400

    update – index & controller/auth

--- api/app/routes/auth.js
+++ api/app/routes/auth.js
@@ -1,5 +1,11 @@
 const router = require('express').Router();
 
+const authCtrl = require("../controllers/auth");
+
+router.get('/', async (req, res) => {
+  console.log("testing api...")
+});
+
 router.post('/signup', authCtrl.signUp);
 
 router.post('/login', authCtrl.login);
