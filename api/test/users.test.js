22a81fee10fc560525e62871f0165a5fa210b5e0
“Tommy” <nh3r717@gmail.com>
2020-10-23 13:53:12 -0400

    update – package & test

--- api/test/users.test.js
+++ api/test/users.test.js
@@ -1,18 +1,33 @@
 const request = require("supertest");
 const app = require("../app");
+const { users } = require("../../api/app/models")
 
 describe("user endpoints", () => {
 
+    // ! make these for each model – user
+
+    let user;
+
+    beforeEach(async () => {
+        user = await users.create({
+            username: "Test",
+            password: "Test",
+        })
+    });
+
+    // ! it.only for one test
+
+    // it.only("update a tag to a user's watched [addToWatched(tagId)]", async () => {
     it("update a tag to a user's watched [addToWatched(tagId)]", async () => {
         const res = await request(app)
             .post('/tags/')
             .send({
-                id: "26d5689d-b15b-4a94-a699-44b3e0fdc401",
+                id: user.id,
                 name: "A Tag"
             })
         // expect(res.body).toHaveProperty("tags");
         // expect(res.body).toHaveProperty("id");
-        // expect(res.statusCode).toEqual(201)
+        // expect(res.statusCode).toEqual(201);
     });
 
     it("update a tag to a user's watched [fetchWatching()]", async () => {
@@ -50,8 +65,14 @@ describe("user endpoints", () => {
             .send({
                 id: "249d28dc-39a8-41db-a0be-1722c64a97c1",
             })
-        // expect(res.body).toHaveProperty("id");
-        // expect(res.statusCode).toEqual(200)
+
+        // ToDO this to all '[]'
+
+        expect(res.body[0]).toHaveProperty("id");
+        expect(res.statusCode).toEqual(200)
+    });
+
+    afterEach(async () => {
+        await users.destroy({ where: { id: user.id } });
     });
 });
-
